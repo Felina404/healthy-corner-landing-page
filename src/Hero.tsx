84c5e9a2f8032a83c6aa59056f1daecb5816ecdb
fsociety00d4t <58801059+fsociety00d4t@users.noreply.github.com>
2025-05-08 16:11:26 +0300

    basic hero layout

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -5,8 +5,19 @@ import heroImg from './assets/hero-img.jpg'
 function Hero() {
 
   return (
-    <div className='w-full round-xl p-4'>
-       <img src={heroImg}></img>
+    <div className='p-8 bg-beige-100'>
+      <div className="relative">
+        <img 
+          src={heroImg} 
+          className="w-full h-full object-cover rounded-xl object-contain md:max-h-[70vh]" 
+          alt="Hero" 
+        />
+        <div className='absolute bottom-0 left-0 p-6 bg-gradient-to-t from-black to-transparent w-full rounded-xl'>
+          <h2 className='text-4xl md:text-5xl lg:text-6xl text-white font-serif w-1/2 lg:w-[30%]'>Where every ingredient matters</h2>
+          <button className='bg-white rounded-2xl p-2 m-2 cursor-pointer hover:bg-green-500'>View Our Menus</button>
+        </div> 
+      </div>
+      
     </div>
      
     
