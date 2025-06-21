87a020c0d68a307d4b55cd14b6d03ed1b959b36f
fsociety00d4t <58801059+fsociety00d4t@users.noreply.github.com>
2025-06-22 01:32:21 +0300

    header menu button link

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -14,7 +14,10 @@ function Hero() {
         />
         <div className='absolute bottom-0 left-0 p-6 bg-gradient-to-t from-black to-transparent w-full rounded-xl'>
           <h2 className='text-4xl md:text-5xl lg:text-6xl text-white font-serif w-1/2 lg:w-[30%]'>Where every ingredient matters</h2>
-          <button className='bg-white rounded-2xl p-2 m-2 cursor-pointer hover:bg-secondary'>View Our Menus</button>
+          <a href="#menu" className=''>
+            <button className='bg-white rounded-2xl p-2 m-2 cursor-pointer hover:bg-secondary'>View Our Menus</button>
+          </a>
+          
         </div> 
       </div>
       
